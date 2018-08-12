cd41bfd625e67119b28bc435a933341136ec4247
Oscar Olazabal <oscarolazabal@gmail.com>
2018-08-12 01:49:15 -0700

    split macos and linux conf

---
+++ setup.sh
@@ -0,0 +1,44 @@
+ln -s tmux/.tmux.conf ~/.tmux.conf
+ln -s vim/.vimrc ~/.vimrc
+ln -s zsh/.zshrc ~/.zshrc
+
+OS="`uname`"
+case $OS in
+    'Linux')
+        ln -s zsh/.zshrc-Linux ~/.zshrc-Linux
+        ln -s vscode/settings.json ~/.config/Code/User/settings.json
+        ln -s vscode/keybindings.json ~/.config/Code/User/keybindings.json
+
+        sudo apt-get install zsh
+        chsh -s $(which zsh)
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+        npm install --global pure-prompt
+
+        sudo apt-get install vim
+        curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        vim -c ":PlugInstall" %~/.vconf -c ":qa"
+
+        sudo apt-get install tmux
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+        tmux source ~/.tmux.conf
+        ;;
+    'Darwin')
+        ln -s zsh/.zshrc-Darwin ~/.zshrc-Darwin
+        ln -s vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+        ln -s vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+
+        brew install zsh zsh-completions
+        chsh -s $(which zsh)
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+        npm install --global pure-prompt
+
+        brew install vim && brew install macvim
+        brew link macvim
+        curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        vim -c ":PlugInstall" %~/.vconf -c ":qa"
+
+        brew install tmux
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+        tmux source ~/.tmux.conf
+        ;;
+esac
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -3,10 +3,6 @@
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 export ZSH=$HOME/.oh-my-zsh
 export PATH=$HOME/anaconda3/bin:$PATH
-export PATH=/Library/Frameworks/Mono.framework/Versions/Current/bin/:${PATH}
-export PATH=$PATH:/usr/local/go/bin
-export PATH="$PATH:$HOME/.rvm/bin"
-export PATH=/Library/TeX/Distributions/.DefaultTeX/Contents/Programs/texbin:$PATH
 
 # Plugins
 ZSH_THEME=""
@@ -26,7 +22,6 @@ plugins=(
 
 # Loading
 source $ZSH/oh-my-zsh.sh  # Oh my zsh
-source ~/.fonts/*.sh      # Font maps
 autoload -U promptinit; promptinit
 prompt pure
 
@@ -36,17 +31,14 @@ if command -v tmux>/dev/null; then
 fi
 
 # Alias
-alias v="mvim"
 alias top="htop"
 alias c="clear"
-alias vconf="mvim ~/.vimrc"
-alias zconf="mvim ~/.zshrc"
-alias tconf="mvim ~/.tmux.conf"
-alias thw="less ~/.tmux/help/windows"
-alias thp="less ~/.tmux/help/panes"
 
 # Functions
 mc()     { mkdir $1; cd $1; }
 cl()     { cd $1; ll; }
 backup() { cp $1{,.bak}; }
+
+# System specific
+source "${ZDOTDIR:-${HOME}}/.zshrc-`uname`"#
 
