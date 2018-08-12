389a0767f9cf6524b8047e260de25b0870225944
Oscar Olazabal <oscarolazabal@gmail.com>
2018-08-12 02:00:31 -0700

    update setup

--- setup.sh
+++ setup.sh
@@ -12,11 +12,6 @@ case $OS in
         ln -s vscode/settings.json ~/.config/Code/User/settings.json
         ln -s vscode/keybindings.json ~/.config/Code/User/keybindings.json
 
-        sudo apt-get install zsh
-        chsh -s $(which zsh)
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-        npm install --global pure-prompt
-
         sudo apt-get install vim
         curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
         vim -c ":PlugInstall" %~/.vconf -c ":qa"
@@ -24,17 +19,17 @@ case $OS in
         sudo apt-get install tmux
         git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
         tmux source ~/.tmux.conf
+
+        sudo apt-get install zsh
+        sudo npm install --global pure-prompt
+        chsh -s $(which zsh)
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
         ;;
     'Darwin')
         rm ~/Library/Application\ Support/Code/User/settings.json
         rm ~/Library/Application\ Support/Code/User/keybindings.json
         ln -s vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
         ln -s vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-
-        brew install zsh zsh-completions
-        chsh -s $(which zsh)
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-        npm install --global pure-prompt
 
         brew install vim && brew install macvim
         brew link macvim
@@ -44,5 +39,10 @@ case $OS in
         brew install tmux
         git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
         tmux source ~/.tmux.conf
+
+        brew install zsh zsh-completions
+        npm install --global pure-prompt
+        chsh -s $(which zsh)
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
         ;;
 esac
