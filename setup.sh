4980017989ebc49c835cc952067607f11f5bb536
Oscar Olazabal <oscarolazabal@gmail.com>
2018-08-12 02:06:47 -0700

    update setup

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
-ln -s tmux/.tmux.conf ~/.tmux.conf
-ln -s vim/.vimrc ~/.vimrc
-ln -s zsh/.zshrc ~/.zshrc
-ln -s zsh/.zshrc-Linux ~/.zshrc-Linux
-ln -s zsh/.zshrc-Darwin ~/.zshrc-Darwin
+ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/dotfiles/zsh/.zshrc-Linux ~/.zshrc-Linux
+ln -s ~/dotfiles/zsh/.zshrc-Darwin ~/.zshrc-Darwin
 
 OS="`uname`"
 case $OS in
     'Linux')
         rm ~/.config/Code/User/setting.json
         rm ~/.config/Code/User/keybindings.json
-        ln -s vscode/settings.json ~/.config/Code/User/settings.json
-        ln -s vscode/keybindings.json ~/.config/Code/User/keybindings.json
+        ln -s ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+        ln -s ~/dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
 
         sudo apt-get install vim
         curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -28,8 +28,8 @@ case $OS in
     'Darwin')
         rm ~/Library/Application\ Support/Code/User/settings.json
         rm ~/Library/Application\ Support/Code/User/keybindings.json
-        ln -s vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-        ln -s vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+        ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+        ln -s ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 
         brew install vim && brew install macvim
         brew link macvim
